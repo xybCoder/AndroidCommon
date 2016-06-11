3d77f84095cc8aa46069e561336189032c13d3c6
xybCoder <xyb520zly@gmail.com>
2016-06-11 13:04:54 +0800

    add adapter

---
+++ library/src/main/java/com/utils/library/adapter/abslistview/MultiItemTypeSupport.java
@@ -0,0 +1,10 @@
+package com.utils.library.adapter.abslistview;
+
+public interface MultiItemTypeSupport<T>
+{
+	int getLayoutId(int position, T t);
+
+	int getViewTypeCount();
+
+	int getItemViewType(int position, T t);
+}
