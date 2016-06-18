cff16d5e5e03918a528667071f375a0bfffeb2bf
xybCoder <xyb520zly@gmail.com>
2016-06-18 20:57:15 +0800

    alter cleanUtil

--- library/src/main/java/com/utils/library/CleanUtil.java
+++ library/src/main/java/com/utils/library/CleanUtil.java
@@ -4,93 +4,94 @@ import android.content.Context;
 import android.os.Environment;
 
 import java.io.File;
+import java.math.BigDecimal;
 
 /**
- * 清理数据
- * Created by dell on 2016/6/18.
+ * 本应用数据清除管理器
+ * 主要功能有清除内/外缓存，清除数据库，清除sharedPreference，清除files和清除自定义目录
  */
 public class CleanUtil {
 
     /**
-     * clean internal cache(/data/data/com.xxx.xxx/cache)
+     * 清除本应用内部缓存(/data/data/com.xxx.xxx/cache)
      *
-     * @param context
+     * @param context 上下文
      */
     public static void cleanInternalCache(Context context) {
-        deleteFilesInDirectory(context.getCacheDir());
+        deleteFilesByDirectory(context.getCacheDir());
     }
 
     /**
-     * clean databases(/data/data/com.xxx.xxx/databases)
+     * 清除本应用所有数据库(/data/data/com.xxx.xxx/databases)
      *
-     * @param context
+     * @param context 上下文
      */
     public static void cleanDatabases(Context context) {
-        deleteFilesInDirectory(new File(context.getFilesDir().getPath()
+        deleteFilesByDirectory(new File(context.getFilesDir().getPath()
                 + context.getPackageName() + "/databases"));
     }
 
     /**
-     * clean databases by name
+     * 清除本应用SharedPreference(/data/data/com.xxx.xxx/shared_prefs)
      *
-     * @param context
-     * @param dbName
+     * @param context 上下文
      */
-    public static void cleanDatabases(Context context, String dbName) {
-        context.deleteDatabase(dbName);
-    }
-
-    /**
-     * clean SharedPreference(/data/data/com.xxx.xxx/shared_prefs)
-     *
-     * @param context
-     */
-    public static void cleanSharedPreferences(Context context) {
-        deleteFilesInDirectory(new File(context.getFilesDir().getPath()
+    public static void cleanSharedPreference(Context context) {
+        deleteFilesByDirectory(new File(context.getFilesDir().getPath()
                 + context.getPackageName() + "/shared_prefs"));
     }
 
     /**
-     * clean file(/data/data/com.xxx.xxx/files)
+     * 按名字清除本应用数据库
      *
-     * @param context
+     * @param context 上下文
+     * @param dbName 数据库名称
      */
-    public static void cleanFiles(Context context) {
-        deleteFilesInDirectory(context.getFilesDir());
+    public static void cleanDatabaseByName(Context context, String dbName) {
+        context.deleteDatabase(dbName);
     }
 
     /**
-     * clean external cache(/mnt/sdcard/android/data/com.xxx.xxx/cache)
+     * 清除/data/data/com.xxx.xxx/files下的内容
      *
-     * @param context
+     * @param context 上下文
+     */
+    public static void cleanFiles(Context context) {
+        deleteFilesByDirectory(context.getFilesDir());
+    }
+
+    /**
+     * 清除外部cache下的内容(/mnt/sdcard/android/data/com.xxx.xxx/cache)
+     *
+     * @param context 上下文
      */
     public static void cleanExternalCache(Context context) {
         if (Environment.getExternalStorageState().equals(
                 Environment.MEDIA_MOUNTED)) {
-            deleteFilesInDirectory(context.getExternalCacheDir());
+            deleteFilesByDirectory(context.getExternalCacheDir());
         }
     }
 
     /**
-     * clean custom cache
+     * 清除自定义路径下的文件，使用需小心，请不要误删。而且只支持目录下的文件删除
      *
-     * @param filePath
+     * @param filePath 文件路径
      */
     public static void cleanCustomCache(String filePath) {
-        deleteFilesInDirectory(new File(filePath));
+        deleteFilesByDirectory(new File(filePath));
     }
 
     /**
-     * clean application datas
+     * 清除本应用所有的数据
      *
-     * @param context
-     * @param filePath
+     * @param context 上下文
+     * @param filePath 文件路径
      */
-    public static void cleanApplicationDatas(Context context, String... filePath) {
+    public static void cleanApplicationData(Context context, String... filePath) {
         cleanInternalCache(context);
         cleanExternalCache(context);
         cleanDatabases(context);
-        cleanSharedPreferences(context);
+        cleanSharedPreference(context);
         cleanFiles(context);
         for (String fp : filePath) {
             cleanCustomCache(fp);
@@ -98,20 +99,84 @@ public class CleanUtil {
     }
 
     /**
-     * delete files in directory
+     * 删除方法 这里只会删除某个文件夹下的文件。 此操作较危险，请慎用；
      *
-     * @param directory
+     *
+     * @param directory 文件夹File对象
      */
-    private static void deleteFilesInDirectory(File directory) {
+    private static void deleteFilesByDirectory(File directory) {
         if (directory != null && directory.exists() && directory.isDirectory()) {
             for (File item : directory.listFiles()) {
+                // delete file and folder. It's dangerous!
                 if (item.isDirectory()) {
-                    deleteFilesInDirectory(item);
+                    deleteFilesByDirectory(item);
                 } else {
                     item.delete();
                 }
             }
         }
+    }
+
+    // 获取文件
+    //Context.getExternalFilesDir() --> SDCard/Android/data/你的应用的包名/files/ 目录，一般放一些长时间保存的数据
+    //Context.getExternalCacheDir() --> SDCard/Android/data/你的应用包名/cache/目录，一般存放临时缓存数据
+    public static long getFolderSize(File file) throws Exception {
+        long size = 0;
+        try {
+            File[] fileList = file.listFiles();
+            for (int i = 0; i < fileList.length; i++) {
+                // 如果下面还有文件
+                if (fileList[i].isDirectory()) {
+                    size = size + getFolderSize(fileList[i]);
+                } else {
+                    size = size + fileList[i].length();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return size;
+    }
+
+    public static String getCacheSize(File file) throws Exception {
+        return getFormatSize(getFolderSize(file));
+    }
+
+    /**
+     * 格式化单位
+     *
+     * @param size
+     * @return
+     */
+    public static String getFormatSize(double size) {
+        double kiloByte = size / 1024;
+        if (kiloByte < 1) {
+            return size + "Byte";
+        }
+
+        double megaByte = kiloByte / 1024;
+        if (megaByte < 1) {
+            BigDecimal result1 = new BigDecimal(Double.toString(kiloByte));
+            return result1.setScale(2, BigDecimal.ROUND_HALF_UP)
+                    .toPlainString() + "KB";
+        }
+
+        double gigaByte = megaByte / 1024;
+        if (gigaByte < 1) {
+            BigDecimal result2 = new BigDecimal(Double.toString(megaByte));
+            return result2.setScale(2, BigDecimal.ROUND_HALF_UP)
+                    .toPlainString() + "MB";
+        }
+
+        double teraBytes = gigaByte / 1024;
+        if (teraBytes < 1) {
+            BigDecimal result3 = new BigDecimal(Double.toString(gigaByte));
+            return result3.setScale(2, BigDecimal.ROUND_HALF_UP)
+                    .toPlainString() + "GB";
+        }
+        BigDecimal result4 = new BigDecimal(teraBytes);
+        return result4.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString()
+                + "TB";
     }
 
 }
