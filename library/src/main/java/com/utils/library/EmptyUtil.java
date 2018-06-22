84765c68b664ee5f9bcb9859e83dbd9693af7311
xybcoder <xyb520zly@gmail.com>
2018-06-22 11:54:08 +0800

    添加MacAddressUtil，EmptyUtil

---
+++ library/src/main/java/com/utils/library/EmptyUtil.java
@@ -0,0 +1,29 @@
+package com.utils.library;
+
+import android.text.TextUtils;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class EmptyUtil {
+
+    public static boolean isEmpty(CharSequence str) {
+        return isNull(str) || str.length() == 0;
+    }
+
+    public static boolean isEmpty(Object[] obj) {
+        return isNull(obj) || obj.length == 0;
+    }
+
+    public static boolean isEmpty(Collection<?> list) {
+        return isNull(list) || list.isEmpty();
+    }
+
+    public static boolean isEmpty(Map<?, ?> map) {
+        return isNull(map) || map.isEmpty();
+    }
+
+    public static boolean isNull(Object o) {
+        return o == null;
+    }
+}
